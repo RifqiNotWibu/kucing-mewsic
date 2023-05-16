589570a6cf6b4b1894379c788cf6fcd76abe9aad
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-16 09:22:10 +0700

    Added onPermissionGranted condition

--- app/src/main/java/com/example/mewsicplayer/MainActivity.java
+++ app/src/main/java/com/example/mewsicplayer/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.mewsicplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ListView allMusicList; // listVIEW
+    ArrayAdapter<String> musicArrayAdapter; // Adapter untuk music list
+    String songs[]; // untuk menyimpan nama musik;
+    ArrayList<File> musics;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        // casting listview
+        allMusicList = findViewById(R.id.listView);
+
+        //dexter permission
+        Dexter.withActivity(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                // menampilkan file musik dalam storage
+                musics = findMusicFiles(Environment.getExternalStorageDirectory());
+                songs = new String[musics.size()];
+                for (int i = 0; i <musics.size(); i++) {
+                    songs[i] = musics.get(i).getName();
+                }
+
 }
