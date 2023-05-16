e338ac73065c7f4dd591af64d83ec77743f615cf
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-05-16 09:59:37 +0700

    Debugging

--- app/src/main/java/com/example/mewsicplayer/MainActivity.java
+++ app/src/main/java/com/example/mewsicplayer/MainActivity.java
@@ -4,11 +4,13 @@ import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
